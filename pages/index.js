beac849cd54e9f6888d9b3e24a30067d2161af01
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-08 13:57:31 -0500

    Update index.js
    hero change

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
              </p>
                           <div className={styles.hero}>
                              <Image 
-                                src="/gods-finger.jpg"
+                                src="/bible-hero.jpg"
                                 alt="Picture of the Gods finger"
                                 height={900}
                                 width={2000}
