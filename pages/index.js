a6fa0a15dbd26c2272309fae0a5f2c6153dd39df
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-08 14:03:14 -0500

    Update index.js
    bible-open.jpg

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
              </p>
                           <div className={styles.hero}>
                              <Image 
-                                src="/bible-hero.jpg"
+                                src="/bible-open.jpg"
                                 alt="Picture of the Gods finger"
                                 height={900}
                                 width={2000}
