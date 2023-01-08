c465ce4e9e49b785b14deff08eb159f889140807
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-08 10:43:50 -0500

    Update index.js
    gods-finger.jpg

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
              </p>
                           <div className={styles.hero}>
                              <Image 
-                                src="/hand-hold-stack-books.webp"
+                                src="/gods-finger.jpg"
                                 alt="Picture of the Gods finger"
                                 height={900}
                                 width={2000}
