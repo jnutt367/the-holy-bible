cf923eb0e57b764dc64ee618468f318846fe9cbb
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-14 23:09:51 -0500

    Update index.js
    New-Testament-1024x683.jpg

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
              </p>
                           <div className={styles.hero}>
                              <Image 
-                                src="/new-bible.jpg"
+                                src="/New-Testament-1024x683.jpg"
                                 alt="Picture of the Gods finger"
                                 height={900}
                                 width={2000}
